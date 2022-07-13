205f8132e8e5ae84855d9d64e0e3cefbdd1b13bb
Tomer Ussishkin <tomer.uss@gmail.com>
2022-07-13 21:30:25 +0300

    Protocol Fine-Tuning

---
+++ CommunicationProtocol/EchatLimits.h
@@ -0,0 +1,18 @@
+#ifndef __ECHATLIMITS_H__
+#define __ECHATLIMITS_H__
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_NAME_LEN 32
+#define MIN_NAME_LEN
+#define MAX_PASSWORD_LEN 32
+#define MIN_PASSWORD_LEN
+#define MAX_GROUP_NAME_LEN 222
+#define MIN_GROUP_NAME_LEN
+#define MAX_IP_LEN 16
+#define MAX_MESSAGE_LEN 258
+
+#define MAX_GROUP_NUM 256
+
+#endif /* __ECHATLIMITS_H__ */
--- CommunicationProtocol/Protocol.h
+++ CommunicationProtocol/Protocol.h
@@ -1,36 +1,92 @@
+#ifndef __PROTOCOL_H__
+#define __PROTOCOL_H__
+#include "EchatLimits.h"
+
+typedef char switch_t;
+
+typedef enum PackResult
+{
+    /* Pack/Unpack Results */
+    PACK_SUCCESS,
+    PACK_FAIL
+} PackResult;
 
 typedef enum Reply
 {
+    /* Protocol Results */
     SUCCESS,
+    /* Registration Protocols Results */
     REG_FAIL_USR_EXIST,
+    /* Login Protocol Results */
     LOGIN_FAIL_ALREADY_ACTIVE,
     LOGIN_FAIL_WRONG_INPUT,
+    /* Group Protocol Results */
     GROUP_FAIL_GROUP_NOT_FOUND,
+    /* Create Group Protocol Results */
     CREATE_GROUP_FAIL_OVERFLOW,
     CREATE_GROUP_FAIL_ILLIGAL_INPUT,
     CREATE_GROUP_FAIL_DUPLICATE,
-    PACK_FAIL
 } Reply;
 
-typedef enum Protocol
+typedef enum ProtocolType
 {
+    /* Registration Protocols */
     REGISTRATION_REQUEST, /* char* Username, char* Password */
     REGISTRATION_REPLY,   /* Reply */
+    /* Login Protocols */
     LOGIN_REQUEST,        /* char* Username, char* Password */
     LOGIN_REPLY,          /* Reply */
     LOGOUT_NOTIFY,        /* char* Username */
+    /* Group Protocols */
     GROUP_JOIN_REQUEST,   /* char* Username, char* GroupName */
     GROUP_JOIN_REPLY,     /* Reply, char* GroupName, char* udpIP */
     GROUP_LEAVE,          /* char* Username, char* GroupName */
     GROUP_LIST_REQUEST,   /*  */
-    GROUP_LIST_REPLY,     /* char numOfGroupsLeft, char* GroupName, int usersInGroup */
+    /* //! WARNING - works as stream - see documentation below */
+    GROUP_LIST_REPLY,     /* unsigned char numOfGroupsLeft, char* GroupName, unsigned int usersInGroup */
+    /* Create Group Protocols */
     CREATE_GROUP_REQUEST, /* char* Username, char* GroupName */
     CREATE_GROUP_REPLY    /* Reply, char* GroupName, char* udpIP */
+} ProtocolType;
+
+typedef struct Protocol
+{
+    ProtocolType m_protocolType;
+    Reply m_reply;
+    char m_name[MAX_NAME_LEN];
+    char m_password[MAX_PASSWORD_LEN];
+    char m_groupName[MAX_GROUP_NAME_LEN];
+    char m_udpIP[MAX_IP_LEN];
+    /* Relevent only to GROUP_LIST_REPLY protocol */
+    unsigned int m_usersInGroup;
+    unsigned char m_numOfGroupsLeft;
+    unsigned char m_offset;
+    switch_t m_finishedMSG;
+    switch_t m_finishedPack;
 } Protocol;
 
-typedef struct ProtocolParams ProtocolParams;
+/**
+ * @brief Packs a message for TCP transmission
+ * 
+ * @param _PackedMessage - pointer to buffer for packed message (Have to be at least MAX_MESSAGE_LEN long!)
+ * @param _params - pointer to protocol parameters - Only members relevent to protocol will be packed (see ProtocolType)
+ * @return PackResult 
+ * @retval PACK_SUCCESS - packed message to buffer
+ * @retval PACK_FAIL - couldn't pack message - if one of the pointers or the relevant members are uninitiallized
+ * 
+ * @warning //! The Protocol Type in the struct must be set to desired protocol
+ */
+PackResult Pack(void *_PackedMessage, const Protocol *_params);
 
-Reply Pack(Protocol _protocol, ProtocolParams _params);
+/**
+ * @brief Unpacks a message received through TCP transmission to Protocol struct
+ * 
+ * @param _PackedMessage - pointer to recieved message
+ * @param _params - pointer to Protocol struct to write message contents to
+ * @return PackResult 
+ * @retval PACK_SUCCESS - unpacked message to buffer 
+ * @retval PACK_FAIL - couldn't unpack message - if one of the pointers or the relevant members are unset
+ */
+PackResult UnPack(const void *_PackedMessage, Protocol *_params);
 
-Reply UnPack(Protocol *_protocolRecieved, ProtocolParams _params);
-
+#endif /* __PROTOCOL_H__ */
